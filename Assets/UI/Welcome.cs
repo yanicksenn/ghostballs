5f25c857a6b1815c2f37db888bd6149bbae43cd0
Christian Zommerfelds <christian.zommerfelds@gmail.com>
2024-01-28 17:08:27 +0100

    start game with controller

--- Assets/UI/Welcome.cs
+++ Assets/UI/Welcome.cs
@@ -7,10 +7,28 @@ public class Welcome : MonoBehaviour
     [SerializeField]
     private SceneSelector sceneSelector;
 
+    private PlayersControls playersControls;
+
+    private void Awake()
+    {
+        playersControls = new PlayersControls();
+    }
+    private void OnEnable()
+    {
+        playersControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playersControls.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        var resetButtonPressed = playersControls.Controls.Reset.ReadValue<float>() == 1.0;
+
+        if (Input.anyKey || resetButtonPressed)
         {
             Debug.Log("A key or mouse click has been detected");
             sceneSelector.LoadFirstLevel();
